24ef6cb90a415985283877ae1d73d07543afd895
trodriguezam <torotegui@gmail.com>
2022-04-19 00:05:12 -0400

    inti

---
+++ Tarea_2_FundEco.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+ALPHA = 0.58   # capital share in agriculture
+BETA = 0.42   # capital share in manufacturing
+KBAR = 100  # Total capital supply
+LBAR = 100  # Total labor supply
+p = 1      # =Pa/Pm  relative price of ag goods
+
+def F(K,L,alpha=ALPHA):
+    """Agriculture Production function"""
+    return (K**alpha)*(L**(1-alpha))
+
+def G(K,L,beta=BETA):
+    """Manufacturing Production function"""
+    return (K**beta)*(L**(1-beta))
+
+def edgeworth(L, Kbar=KBAR, Lbar=LBAR,alpha=ALPHA, beta=BETA):
+    """efficiency locus: """
+    a = (1-alpha)/alpha
+    b = (1-beta)/beta
+    return b*L*Kbar/(a*(Lbar-L)+b*L)
+
+def ppf(LA,Kbar=KBAR, Lbar=LBAR,alpha=ALPHA,beta=BETA):
+    """Draw a production possibility frontier
+    
+    arguments:
+    LA -- labor allocated to ag, from which calculate QA(Ka(La),La) 
+    """
+    KA = edgeworth(LA, Kbar, Lbar,alpha, beta)
+    RTS = (alpha/(1-alpha))*(KA/LA)
+    QA = F( KA,LA,alpha)
+    QM = G(Kbar-KA,Lbar-LA,beta)
+    ax.scatter(QA,QM)
+    La = np.arange(0,Lbar)
+    Ka = edgeworth(La, Kbar, Lbar,alpha, beta)
+    Qa = F(Ka,La,alpha)
+    Qm = G(Kbar-Ka,Lbar-La,beta)
+    ax.set_xlim(0, Lbar)
+    ax.set_ylim(0, Kbar)
+    ax.plot(Qa, Qm,'k-')
+    ax.set_xlabel(r'$Q_A$',fontsize=18)
+    ax.set_ylabel(r'$Q_M$',fontsize=18)
+    plt.show()
+
+fig, ax = plt.subplots(figsize=(7,6))
+ppf(30,alpha=0.8,beta=0.5)
